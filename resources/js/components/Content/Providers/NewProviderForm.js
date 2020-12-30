d19d4c2d41f7a92d0e2cd718bc33dc151d16a802
Artem Zharikov <7383125@mail.ru>
2020-12-30 10:36:16 +0300

    formik 3

--- resources/js/components/Content/Providers/NewProviderForm.js
+++ resources/js/components/Content/Providers/NewProviderForm.js
@@ -41,7 +41,7 @@ const NewProviderForm = (props) => {
     // });
 
     function onChangeLoadType(e, field, values, setValues) {
-        let valuesCopy = {...values};
+        let valuesCopy = { ...values };
         valuesCopy.data = [];
         valuesCopy.loadType = e.target.value;
         if (e.target.value === 'mail') {
@@ -52,6 +52,7 @@ const NewProviderForm = (props) => {
         if (e.target.value === 'api') {
             valuesCopy.mailData = {};
             valuesCopy.data.push('apiAdress')
+            valuesCopy.data.push('getType')
         }
         setValues(valuesCopy)
 
@@ -64,7 +65,7 @@ const NewProviderForm = (props) => {
     }
 
     return (
-        <Formik initialValues={initialValues}  onSubmit={onSubmit}>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
             {({ errors, values, touched, setValues }) => (
                 <Form>
                     <Field
@@ -75,70 +76,57 @@ const NewProviderForm = (props) => {
                         validate={validateName}
                         hasFeedback
                     />
-                    {/* <div className="card m-3">
-                        <div className="card-body border-bottom">
-                            <div className="form-row">*/}
-                                <div className="form-group ant-row">
-                                    <div className="ant-col ant-form-item-label"><label>Способ загрузки прайса</label> </div>
-                                    <div className="ant-form-item-control">
-                                        <Field name="LoadType">
-                                        {({ field }) => (
-                                            <select 
-                                                {...field} 
-                                                className={'form-control' + (errors.numberOfTickets && touched.numberOfTickets ? ' is-invalid' : '')} 
-                                                onChange={e => onChangeLoadType(e, field, values, setValues)}
-                                            >
-                                                <option value=""></option>
-                                                {   [
-                                                    {value: 'api', label:'API', disabled: false},
-                                                    {value: 'mail', label:'Email', disabled: false},
-                                                    {value: 'ftp', label:'FTP', disabled: true},
-                                                    {value: 'parser', label:'Parser', disabled: false},
-                                                    ].map(i => 
-                                                    <option key={i.value} value={i.value} disabled={i.disabled}>{i.label}</option>
-                                                )}
-                                            </select>
+                    <div className="form-group ant-row">
+                        <div className="ant-col ant-form-item-label"><label>Способ загрузки прайса</label> </div>
+                        <div className="ant-form-item-control">
+                            <Field name="LoadType">
+                                {({ field }) => (
+                                    <select
+                                        {...field}
+                                        className={'form-control' + (errors.numberOfTickets && touched.numberOfTickets ? ' is-invalid' : '')}
+                                        onChange={e => onChangeLoadType(e, field, values, setValues)}
+                                    >
+                                        <option value=""></option>
+                                        {[
+                                            { value: 'api', label: 'API', disabled: false },
+                                            { value: 'mail', label: 'Email', disabled: false },
+                                            { value: 'ftp', label: 'FTP', disabled: true },
+                                            { value: 'parser', label: 'Parser', disabled: false },
+                                        ].map(i =>
+                                            <option key={i.value} value={i.value} disabled={i.disabled}>{i.label}</option>
                                         )}
-                                        </Field>
-                                    </div>
-                                    <ErrorMessage name="numberOfTickets" component="div" className="invalid-feedback" />
-                                 </div>
-                            {/*</div>
-                        </div> */}
-                        <FieldArray name="params">
+                                    </select>
+                                )}
+                            </Field>
+                        </div>
+                        <ErrorMessage name="numberOfTickets" component="div" className="invalid-feedback" />
+                    </div>
+                    <FieldArray name="params">
                         {() => (values.data.map((ticket, i) => {
-                            console.log('ticket'+i, ticket)
+                            console.log('ticket' + i, ticket)
                             const ticketErrors = errors.tickets?.length && errors.tickets[i] || {};
                             const ticketTouched = touched.tickets?.length && touched.tickets[i] || {};
                             return (
-                                <div key={i} className="list-group list-group-flush">
-                                    <div className="list-group-item">
-                                        {/* <h5 className="card-title">Ticket {i + 1}</h5> */}
-                                        <div className="form-row">
-                                            {/* <div className="form-group col-6"> */}
-                                            {/* <div className="form-group"> */}
-                                                <label className="ant-form-item-label">{ticket}</label>
-                                                <Field name={ticket} type="text" className={'form-control col-8' + (ticketErrors.name && ticketTouched.name ? ' is-invalid' : '' )} />
-                                                <ErrorMessage name={`tickets.${i}.name`} component="div" className="invalid-feedback" />
-                                            {/* </div> */}
-                                            {/* <div className="form-group col-6">
-                                                <label>Email</label>
-                                                <Field name={`tickets.${i}.email`} type="text" className={'form-control' + (ticketErrors.email && ticketTouched.email ? ' is-invalid' : '' )} />
-                                                <ErrorMessage name={`tickets.${i}.email`} component="div" className="invalid-feedback" />
-                                            </div> */}
-                                        </div>
+                                <div className="form-group ant-row">
+                                    <div className="ant-col ant-form-item-label"><label className="ant-form-item-label">{ticket}</label></div>
+                                    <div className="ant-form-item-control">
+                                        <Field name={ticket} type="text" className={'form-control' + (ticketErrors.name && ticketTouched.name ? ' is-invalid' : '')} />
+                                        <ErrorMessage name={`tickets.${i}.name`} component="div" className="invalid-feedback" />
                                     </div>
                                 </div>
                             );
                         }))}
-                        </FieldArray>
-                        {/* <div className="card-footer text-center border-top-0"> */}
-                            <button type="submit" className="btn btn-primary mr-1">
-                                Buy Tickets
-                            </button>
-                            <button className="btn btn-secondary mr-1" type="reset">Reset</button>
-                        {/* </div> */}
-                    {/* </div> */}
+                    </FieldArray>
+                    <button type="submit" className="btn btn-primary mr-1">
+                        Buy Tickets
+                    </button>
+                    <button className="btn btn-secondary mr-1" type="reset">Reset</button>
+                    <li>Допилить обьект loadType</li>
+                    <li>switch</li>
+                    <li>Валидация</li>
+                    <li>Api для создания поставщиков</li>
+                    <li>Проверка на существование</li>
+                    <li>Валидация имени таблицы</li>
                 </Form>
             )}
         </Formik>
