a0b441a35dedfac26ed6f00ed8e15e86b5859cd1
Artem Zharikov <7383125@mail.ru>
2021-01-19 16:30:19 +0300

    456

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,11 +18,16 @@ class TaskController extends Controller
             "Tasks"=> Task::all()
             ], 200);
 
-        return $req
-            ->header('Access-Control-Allow-Origin', '*')
+        
+
+        $req ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
             ->header('Access-Control-Allow-Credentials',' true');
+
+        var_dump($req->headers);
+
+        return $req;
     }
 
     /**
