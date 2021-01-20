622255db68359bda5cd7595c8c7f65b93f6bae21
Artem Zharikov <7383125@mail.ru>
2021-01-20 14:01:07 +0300

    3

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -31,7 +31,8 @@ class TaskController extends Controller
         //     ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
         //     ->header('Access-Control-Allow-Credentials',' true');
 
-        // var_dump($req->headers);
+        // var_dump($req->headers)
+        ;
 
         return $req;
     }
