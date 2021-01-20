ee9211fd72d3320172417682c3e578935fc7c29b
Artem Zharikov <7383125@mail.ru>
2021-01-20 12:26:44 +0300

    cors1

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
         'allow_credentials' => false,
 
         'allow_origins' => [
-            '*',
+            // '*',
+            'https://localhost:3000/'
         ],
 
         'allow_methods' => [
