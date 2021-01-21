c1850284e39d33fc5a7fa0d90712e6ea636d780e
Artem Zharikov <7383125@mail.ru>
2021-01-21 14:24:48 +0300

    10

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ if ( in_array($request_headers['Host'], $trusted_adress) ) {
 }
 
 // header('Access-Control-Allow-Origin: '.$request_headers['Host']);
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://zharikovartem.github.io/epam-app/');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
