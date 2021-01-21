da7b2e2b578a791116ee8f0790680193e36215e1
Artem Zharikov <7383125@mail.ru>
2021-01-21 14:10:25 +0300

    7

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ if ( in_array($request_headers['Host'], $trusted_adress) ) {
     $adress = '*';
 }
 
-header('Access-Control-Allow-Origin: '.$adress);
+header('Access-Control-Allow-Origin: '.$request_headers['Host']);
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
