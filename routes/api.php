44b2d1559da37edacaf9a5d5b6b14d9ec1611507
Artem Zharikov <7383125@mail.ru>
2021-02-04 11:36:02 +0300

    x-auth-token2

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ if ( isset($_SERVER['HTTP_ORIGIN'])) {
 } else {
     $http_origin = '*';
 }
-header('Access-Control-Allow-Origin:'.$http_origin.', x-auth-token');
+header('Access-Control-Allow-Origin:'.$http_origin); //.', x-auth-token');
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
+header('Access-Control-Allow-Headers: origin, x-requested-with, content-type, x-auth-token');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
 
 /*
