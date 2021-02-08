153465f8b201f3d280c09bb8ed5845fbafa1714c
Artem Zharikov <7383125@mail.ru>
2021-02-08 15:21:50 +0300

    2

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -87,7 +87,7 @@ class TaskController extends Controller
     {
         $fields = $request->all();
         foreach ($fields as $field => $value) {
-            if (isset($task[$field]) || $taskList[$field]===null) { 
+            if (isset($task[$field]) || $task[$field]===null) { 
                 $task[$field] = $value;
             } else {
                 $message[$field] = 'do not exist';
