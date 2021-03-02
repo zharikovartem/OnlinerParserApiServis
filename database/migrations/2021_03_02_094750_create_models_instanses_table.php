a920de6165c5adc6f9c2095e9c417e8f7e92d4a4
Artem Zharikov <7383125@mail.ru>
2021-03-02 12:51:21 +0300

    Models_instanses

---
+++ database/migrations/2021_03_02_094750_create_models_instanses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelsInstansesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Models_instanses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->text('name');
+            $table->json('fields');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Models_instanses');
+    }
+}
