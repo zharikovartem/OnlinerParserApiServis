439b6cf5a31a021aa0dd21ae6b72fea1812cf419
Artem Zharikov <7383125@mail.ru>
2021-03-17 17:59:05 +0300

    where

--- app/Controllers.php
+++ app/Controllers.php
@@ -38,7 +38,7 @@ class Controllers extends Model
             // echo 'isResource: controller-Id: '.$this->id;
             // $childMethods = ControllerMethods::where('controller_id', $this->id)->get();
             // var_dump($childMethods);
-            $childMethods = DB::table('ControllerMethods')->whereIn('controller_id', $this->id)->get();
+            $childMethods = DB::table('ControllerMethods')->where('controller_id', $this->id)->get();
             // var_dump($childMethods);
 
             $methods = [];
