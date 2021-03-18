b66df64610e68bf97c8c687e59b32ae80220b0fc
Artem Zharikov <7383125@mail.ru>
2021-03-18 12:32:37 +0300

    2

--- app/Controllers.php
+++ app/Controllers.php
@@ -130,9 +130,9 @@ class Controllers extends Model
                     'request'=> json_encode([$request, $model]),
                     'body_actions'=>
                         '       foreach ($request->all() as $field => $value) {'.
-                        '           $'.lcfirst( explode('Controller', $this->name)[0] ).'[$field] = $value;'.
-                        '       }'.
-                        '       $'.lcfirst( explode('Controller', $this->name)[0] ).'->save();',
+                        '           $'.lcfirst( explode('Controller', $this->name)[0] ).'[$field] = $value;
+                        '.'       }
+                        '.'       $'.lcfirst( explode('Controller', $this->name)[0] ).'->save();',
                     'response'=> '{
                             "type": "method",
                             "methodId": '.$methodId.',
