b64c8da232cd3bb74a3e4e85bad7fd6fff932bc5
Artem Zharikov <7383125@mail.ru>
2021-03-19 17:14:18 +0300

    tr

--- app/Services/UniversalParser/UniversalParser.php
+++ app/Services/UniversalParser/UniversalParser.php
@@ -29,33 +29,40 @@ class UniversalParser {
 
             echo '<br/>'.$postFields.'<br/>';
 
-            $curl = curl_init();
+            include_once 'Yandex_Translate.php';
+            $pairs = $translator->yandexGetLangsPairs();
+            print_r($pairs);
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"x-rapidapi-host: microsoft-translator-text.p.rapidapi.com",
-		"x-rapidapi-key: a20a4f686emshd81a4a63d9f86cap1dca45jsn16cfe3f7ef53"
-	],
-]);
+            // $curl = curl_init();
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+            // curl_setopt_array($curl, [
+            //     CURLOPT_URL => "https://google-translate1.p.rapidapi.com/language/translate/v2",
+            //     CURLOPT_RETURNTRANSFER => true,
+            //     CURLOPT_FOLLOWLOCATION => true,
+            //     CURLOPT_ENCODING => "",
+            //     CURLOPT_MAXREDIRS => 10,
+            //     CURLOPT_TIMEOUT => 30,
+            //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            //     CURLOPT_CUSTOMREQUEST => "POST",
+            //     CURLOPT_POSTFIELDS => "q=Hello%2C%20world!&source=en&target=es",
+            //     CURLOPT_HTTPHEADER => [
+            //         "accept-encoding: application/gzip",
+            //         "content-type: application/x-www-form-urlencoded",
+            //         "x-rapidapi-host: google-translate1.p.rapidapi.com",
+            //         "x-rapidapi-key: a20a4f686emshd81a4a63d9f86cap1dca45jsn16cfe3f7ef53"
+            //     ],
+            // ]);
 
-curl_close($curl);
+            // $response = curl_exec($curl);
+            // $err = curl_error($curl);
 
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
-}
+            // curl_close($curl);
+
+            // if ($err) {
+            //     echo "cURL Error #:" . $err;
+            // } else {
+            //     echo $response;
+            // }
 
             echo '<br/>';
             $item = new Vocabulary([
