9c4a79d043ccacbb617099dcd9d29c36726e1b93
Artem Zharikov <7383125@mail.ru>
2021-03-19 16:32:48 +0300

    curl

--- app/Services/UniversalParser/UniversalParser.php
+++ app/Services/UniversalParser/UniversalParser.php
@@ -21,13 +21,39 @@ class UniversalParser {
             echo trim($colls[2]->find('span')[0]->html()); 
             echo '='.trim($colls[4]->html()).'('.trim($colls[6]->html()).')';
 
-            $yandex_url = explode('ru-en', trim( $colls[5]->find('a')[0]->getAttribute('href') ))[0].'en-ru';
-            $yandexDocument = new Document($yandex_url, true);
-            
-            echo $yandexDocument->html();
-            // var_dump($yandexDocument);
-            // var_dump( $yandexDocument->find('.dictionary-pos') );
-            // $part_of_speech = $yandexDocument->find('.dictionary-pos')[0]->getAttribute('title');
+            $curl = curl_init();
+            $postFields = '[{"Text":"'.trim( $colls[2]->find('span')[0]->text() ).'"}]';
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://microsoft-translator-text.p.rapidapi.com/Detect?api-version=3.0",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => $postFields,
+                CURLOPT_HTTPHEADER => [
+                    "content-type: application/json",
+                    "x-rapidapi-host: microsoft-translator-text.p.rapidapi.com",
+                    "x-rapidapi-key: a20a4f686emshd81a4a63d9f86cap1dca45jsn16cfe3f7ef53"
+                ],
+            ]);
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                echo $response;
+            }
+
+
+            $google = 'http://translate.google.ru/translate_a/t?client=x&text={textToTranslate}&hl=en&sl=en&tl=ru';
 
             echo '<br/>';
             $item = new Vocabulary([
