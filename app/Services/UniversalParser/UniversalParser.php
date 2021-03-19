12aade11f4edba398704188928707794c3cb67bb
Artem Zharikov <7383125@mail.ru>
2021-03-19 18:46:35 +0300

    if

--- app/Services/UniversalParser/UniversalParser.php
+++ app/Services/UniversalParser/UniversalParser.php
@@ -31,16 +31,19 @@ class UniversalParser {
 
             // $url= 'https://www.babla.ru/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/'.trim( $colls[2]->find('span')[0]->text() );
             $url= 'https://www.babla.ru/английский-русский/'.trim( $colls[2]->find('span')[0]->text() );
-            try {
-                $ya = new Document($url, true);
-            } catch (Exception $e) {
-                echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+            if (trim( $colls[2]->find('span')[0]->text() ) !== 'I') {
+                try {
+                    $ya = new Document($url, true);
+                } catch (Exception $e) {
+                    echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+                }
+                // echo $ya->html();
+                if ($ya) {
+                    $part_of_speech1 = $ya->find('.quick-result-option')[0]->find('span');
+                    $part_of_speech = $part_of_speech1[0]->text();
+                }
             }
-            // echo $ya->html();
-            if ($ya) {
-                $part_of_speech1 = $ya->find('.quick-result-option')[0]->find('span');
-                $part_of_speech = $part_of_speech1[0]->text();
-            }
+            
             
 
             // var_dump($part_of_speech1[0]->tex);
