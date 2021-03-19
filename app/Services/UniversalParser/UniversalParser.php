c415311d571a5dc5c164b915054af98e3987f8c1
Artem Zharikov <7383125@mail.ru>
2021-03-19 18:40:17 +0300

    check

--- app/Services/UniversalParser/UniversalParser.php
+++ app/Services/UniversalParser/UniversalParser.php
@@ -27,17 +27,21 @@ class UniversalParser {
 
             $yandex_url = explode('ru-en', $colls[5]->find('a')[0]->href)[0].'en-ru' ; 
 
+            $part_of_speech = '?';
 
             // $url= 'https://www.babla.ru/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/'.trim( $colls[2]->find('span')[0]->text() );
             $url= 'https://www.babla.ru/английский-русский/'.trim( $colls[2]->find('span')[0]->text() );
-
             $ya = new Document($url, true);
             // echo $ya->html();
-            $part_of_speech1 = $ya->find('.quick-result-option')[0]->find('span');
+            if ($ya) {
+                $part_of_speech1 = $ya->find('.quick-result-option')[0]->find('span');
+                $part_of_speech = $part_of_speech1[0]->text();
+            }
+            
 
             // var_dump($part_of_speech1[0]->tex);
 
-            $part_of_speech = $part_of_speech1[0]->text();
+            
 
             // echo '<h1>!!!'.$part_of_speech.'</h1>';
 
