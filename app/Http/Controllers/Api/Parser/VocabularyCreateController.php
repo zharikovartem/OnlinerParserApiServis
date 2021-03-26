75abe92b0e063556211c48bba7bfbfdb644341b5
Artem Zharikov <7383125@mail.ru>
2021-03-26 13:26:38 +0300

    123

--- app/Http/Controllers/Api/Parser/VocabularyCreateController.php
+++ app/Http/Controllers/Api/Parser/VocabularyCreateController.php
@@ -24,7 +24,8 @@ class VocabularyCreateController extends Controller
     {
         # php artisan make:job VocabularyParsingJob // создать Job
         // dispatch(new VocabularyParsingJob);
-        
+
+        dispatch( (new VocabularyParsingJob(1 ,500, 0)) );
         // dispatch((new VocabularyParsingJob(1 ,500, 0))->onQueue('vocabulary'));
 
         $newParser = new VocabularyParser(1, 500, 0);
