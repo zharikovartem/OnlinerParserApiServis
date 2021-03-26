2e3328d3557058802876e22c4671a63a32b3a13b
Artem Zharikov <7383125@mail.ru>
2021-03-26 14:48:36 +0300

    333

--- app/Services/Vocabulary/VocabularyParser.php
+++ app/Services/Vocabulary/VocabularyParser.php
@@ -64,6 +64,7 @@ class VocabularyParser
             if ($this->part !== 4) {
                 dispatch( (new VocabularyParsingJob($this->start ,$this->stop, $this->part+1)) );
             } else {
+                echo '???????????????????????????';
                 echo $this->start+500 .', '. $this->stop+500 .', 0 <br/>';
                 echo $this->url.'<br/>';
                 dispatch( (new VocabularyParsingJob($this->start+500 ,$this->stop+500, 0)) );
