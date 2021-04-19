293c71c26811957efd6d6b02e34565ce9b425f8f
zharikovartem <42375825+zharikovartem@users.noreply.github.com>
2021-04-19 21:24:52 +0300

    2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler 
 {
+    use Throwable;
+    
     /**
      * A list of the exception types that are not reported.
      *
